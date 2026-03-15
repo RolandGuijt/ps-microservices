51223cdc511c51155be34c0c44599a3d7e98e074
Roland <roland@duendesoftware.com>
2026-03-15 11:48:39 +0100

    Add shipping

---
+++ GloboTicket/m5/GloboTicket.Services.Shipping/ShipOrderHandler.cs
@@ -0,0 +1,13 @@
+﻿using GloboTicket.Integration.Messages;
+using Microsoft.Extensions.Logging;
+
+namespace GloboTicket.Services.Shipping;
+
+class ShipOrderHandler(ILogger<ShipOrderHandler> log) : IHandleMessages<ShipOrder>
+{
+    public Task Handle(ShipOrder message, IMessageHandlerContext context)
+    {
+        log.LogInformation("Order [{OrderId}] - Successfully shipped.", message.OrderId);
+        return Task.CompletedTask;
+    }
+}
